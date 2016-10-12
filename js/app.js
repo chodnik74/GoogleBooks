0e3faaeb6f727f90f4bc6350621ab61375b838d4
Ondřej Ochodnický <chodnik74@gmail.com>
2016-10-12 18:33:37 +0200

    Oprava clearfix

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ $( document ).ready(function() {
     var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=javascript&maxResults=10&startIndex=";
     var $books = $("#books");
     var indexBook = 0;
+    var clearfixIndex = 0;
     var block = false;
- 
     function getBooks() {
         // Zabrání opětovnému volání funkce před dokončením AJAXu
         if (block) return;
@@ -17,19 +17,20 @@ $( document ).ready(function() {
             var volumeInfo;
             var item;
             var clearfix;
-            // nebo v jednom řádku:
-            //var volumeInfo, item, clearfix;
+            if (response.items.length < 10) {
+                alert("Error in load 10 items");
+            }
  
             for (var i = 0; i < response.items.length; i++) {
               item = response.items[i];
               volumeInfo = item.volumeInfo;
               clearfix = "";
- 
-              if (i % 2 == 0) {
+              if (indexBook % 2 == 0) {
                 clearfix = "<div class='clearfix visible-sm visible-md visible-lg'></div>";
               } else {
                 clearfix = "";
               }
+              indexBook++;
  
               $books.html( $books.html() + clearfix + "<div class='col-xs-12 col-sm-6'>" +
                 "<div class='books-item'>" +
