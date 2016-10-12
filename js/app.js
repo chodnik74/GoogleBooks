5b6b6abd2a71cb54d8d52c330534ce83446197e8
Ondřej Ochodnický <chodnik74@gmail.com>
2016-10-12 17:41:53 +0200

    Řešení chyby načítání

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=javascript&maxResults=9&startIndex=";
+    var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=javascript&maxResults=10&startIndex=";
     var $books = $("#books");
     var indexBook = 0;
     var block = false;
@@ -10,7 +10,7 @@ $( document ).ready(function() {
  
         // Zablokuje funkci
         block = true;
- 
+        //alert(indexBook);
         var googlebooksAPI = apiUrl + indexBook;
  
         $.getJSON(googlebooksAPI, function(response) {
@@ -25,18 +25,23 @@ $( document ).ready(function() {
               volumeInfo = item.volumeInfo;
               clearfix = "";
  
-              if (i % 3 == 0) {
+              if (i % 2 == 0) {
                 clearfix = "<div class='clearfix visible-sm visible-md visible-lg'></div>";
+              } else {
+                clearfix = "";
               }
  
-              $books.html( $books.html() + clearfix + "<div class='col-xs-12 col-sm-4'>" +
+              $books.html( $books.html() + clearfix + "<div class='col-xs-12 col-sm-6'>" +
                 "<div class='books-item'>" +
                 "<div class='books-item-title'>" + (volumeInfo.title ? volumeInfo.title : "- / -")  + "</div>" +
                 "<div class='books-item-images'>" + (volumeInfo.imageLinks ? ("<img src='" + volumeInfo.imageLinks.thumbnail + "'>") : "no thumbnail") + "</div>" +
                 "</div>" +
                 "</div>");
             }
-            indexBook += 9;
+            console.log(response);
+            //console.log("Old index: " + indexBook);
+            indexBook += 10;
+            //console.log("News index: " + indexBook);
         }).always(function() {
             // Odblokuje funkci po dokončení dotazu nebo při chybě
             block = false;
