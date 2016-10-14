c831c557b4af0193f629e09b99f7d50804bb7fc9
Ondřej Ochodnický <chodnik74@gmail.com>
2016-10-14 19:09:08 +0200

    Menší optimalizace

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var windowHeight = $(window).height();
     var bodyHeight = body.height();
     var searchWord = "javascript";
-    
+
     function blockBtn() {
         block = true;
         $btn.addClass("disabled");
@@ -32,13 +32,18 @@ $(document).ready(function() {
 
         if (count == 1) count = 2; // Google nechce vracet pouze jednu položku
 
-        $.getJSON("https://www.googleapis.com/books/v1/volumes?q="+ searchWord +"&key=AIzaSyAXdxLtzZneUFXbWDt_TjbJMKhEIdGcYcQ&maxResults=" + count + "&startIndex=" + index, function(response) {
+        $.getJSON("https://www.googleapis.com/books/v1/volumes?q=" + searchWord + "&key=AIzaSyAXdxLtzZneUFXbWDt_TjbJMKhEIdGcYcQ&maxResults=" + count + "&startIndex=" + index, function(response) {
             var volumeInfo;
             var item;
             var clearfix;
             var responseItemCount = 0;
-
-            console.log(response);
+            var booksInfo = document.getElementById("books-template").innerHTML;
+            var template = Handlebars.compile(booksInfo);
+            var bookItems = [];
+            var bookTitle;
+            var bookDescFull;
+            var bookDesc;
+            var bookImg;
 
             if (response.items && response.items.length > 0) {
                 responseItemCount = response.items.length;
@@ -49,27 +54,30 @@ $(document).ready(function() {
                 }
             }
 
-            var booksInfo = document.getElementById("books-template").innerHTML;
-            var template = Handlebars.compile(booksInfo);
-            var bookItems = [];
             for (var i = 0; i < responseItemCount; i++) {
                 indexBook++;
                 item = response.items[i];
                 volumeInfo = item.volumeInfo;
                 clearfix = "";
-                var bookTitle;
-                var bookDescFull;
-                var bookDesc;
-                var bookImg;
                 bookTitle = (volumeInfo.title ? volumeInfo.title : "No title");
                 bookDescFull = (volumeInfo.description ? volumeInfo.description : "No description");
-                bookDesc = bookDescFull.substring(0,200);
+                bookDesc = bookDescFull.substring(0, 200);
                 bookDesc = bookDesc + "...";
-                bookImg =  (volumeInfo.imageLinks ? ("<img src='" + volumeInfo.imageLinks.thumbnail + "'>") : "No thumbnail");
+                bookImg = (volumeInfo.imageLinks ? ("<img src='" + volumeInfo.imageLinks.thumbnail + "'>") : "No thumbnail");
                 if (indexBook % 2 == 0) {
-                    bookItems.push({title: bookTitle, description: bookDesc, img: bookImg, clearfix: true});
+                    bookItems.push({
+                        title: bookTitle,
+                        description: bookDesc,
+                        img: bookImg,
+                        clearfix: true
+                    });
                 } else {
-                    bookItems.push({title: bookTitle, description: bookDesc, img: bookImg, clearfix: false});
+                    bookItems.push({
+                        title: bookTitle,
+                        description: bookDesc,
+                        img: bookImg,
+                        clearfix: false
+                    });
                 }
             }
             console.log(bookItems);
@@ -106,6 +114,6 @@ $(document).ready(function() {
         if ((offset.top - toBottom) < 500) {
             getBooks();
         }
-    });    
+    });
     getBooks();
 });
